16af6d34417c95c89c8710e8e51b8a4d6ab151c8
brodyns <118648344+brodyns@users.noreply.github.com>
2023-12-08 21:00:35 +0800

    41.缺失的第一个正数

---
+++ Main.java
@@ -0,0 +1,25 @@
+public class Main {
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 0};
+        System.out.println((new Solution()).firstMissingPositive(nums));
+    }
+}
+
+class Solution{
+    public int firstMissingPositive(int[] nums){
+       int n = nums.length;
+        for (int i = 0; i < n; i++) {
+            while(nums[i] > 0 && nums[i] < n && nums[nums[i]-1] != nums[i]){
+                int temp = nums[nums[i-1]];
+                nums[nums[i-1]] = nums[i];
+                nums[i] = temp;
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            if(nums[i] != i+1){
+                return i+1;
+            }
+        }
+        return n+1;
+    }
+}
